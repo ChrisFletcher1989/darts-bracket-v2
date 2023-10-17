642590a7fea8cd123f1e71e0e825b33987d66497
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-10-17 17:53:18 +0900

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Players} from './Components/Players.jsx'
+import Players from './Components/Players.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [playerNumbers, setPlayerNumbers]=useState('')
+  const [playerNames, setPlayerNames]=useState([]);
 
-  return (
-    <>
+  return ( 
       <div>
         <Players/>
         </div>
-    </>
   )
 }
 
