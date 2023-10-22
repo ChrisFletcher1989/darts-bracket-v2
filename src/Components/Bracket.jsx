94b52bb9dd1cb10558f3a9046d75e9be28f5fd2f
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-10-22 15:06:23 +0900

    changes-to-bracket.jsx-from-vanillaJS

--- src/Components/Bracket.jsx
+++ src/Components/Bracket.jsx
@@ -6,10 +6,22 @@ function Bracket ({randomNames, randomClicked}){
     useEffect(() =>{
 console.log("EFFECT", randomNames)
     }, [randomClicked])
+    //VANILLA JS TO MAKE FIRST BRACKET(BEFORE ADDING NAMES)
+    // if(a>=1){
+    //     for (let i = 1; i <= a; i++) {
+    // let bracket = document.createElement("div");
+    // bracket.setAttribute("class", "box"); 
+    // let paragraph = document.createElement("p");
+    // paragraph.setAttribute("class", "playerLabel1")
+    // paragraph.textContent = "Awaiting player info";
+    // bracket.appendChild(paragraph);    
+    // first.appendChild(bracket)
+    //     }
+    // }
     return (
         <div className="players">
         <div className="wrapper">
-       <div id="first" className="zone gray first">{randomNames}</div>
+       <div id="first" className="zone gray first"></div>
        <div id="second" className="zone gray round"></div>
        <div id="third" className="zone gray round"></div>
        <div id="fourth" className="zone gray round"></div>
